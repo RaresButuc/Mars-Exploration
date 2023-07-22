6d192c5c874499253b51fb67cdd46358bdb707b2
appuser <appuser@journey2-86bff4fbf7-ljkb2>
2023-07-22 20:47:12 +0000

    starter project

---
+++ src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
@@ -0,0 +1,15 @@
+package com.codecool.marsexploration.mapexplorer;
+
+import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
+
+public class Application {
+    private static final String workDir = "src/main";
+
+    public static void main(String[] args) {
+        String mapFile = workDir + "/resources/exploration-0.map";
+        Coordinate landingSpot = new Coordinate(6, 6);
+
+        // Add your code here
+    }
+}
+
