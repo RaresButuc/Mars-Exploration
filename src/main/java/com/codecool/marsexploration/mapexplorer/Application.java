8f4ee03023bf293a7d69f4d1a1c2ca0242a63353
RaresButuc <116391767+RaresButuc@users.noreply.github.com>
2023-07-24 13:46:19 +0300

    check adjacent and current

--- src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
@@ -1,5 +1,6 @@
 package com.codecool.marsexploration.mapexplorer;
 
+import com.codecool.marsexploration.mapexplorer.configuration.ConfigurationValidatorImpl;
 import com.codecool.marsexploration.mapexplorer.maploader.MapLoader;
 import com.codecool.marsexploration.mapexplorer.maploader.MapLoaderImpl;
 import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
@@ -13,9 +14,12 @@ public class Application {
     public static void main(String[] args) {
         String mapFile = workDir + "/resources/exploration-0.map";
         Coordinate landingSpot = new Coordinate(6, 6);
-        MapLoader mapLoader= new MapLoaderImpl();
-        System.out.println(mapLoader.load(mapFile).toString());
+//        MapLoader mapLoader= new MapLoaderImpl();
+//        System.out.println(mapLoader.load(mapFile).toString());
         // Add your code here
+        String l = "      l                    ";
+//        String [][] l = [[" "," "," "],["l"," "," "]];
+        System.out.println(new ConfigurationValidatorImpl().checkLandingSpots(l, new Coordinate(1, 1)));
     }
 
 }
--- src/main/java/com/codecool/marsexploration/mapexplorer/configuration/ConfigurationValidatorImpl.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/configuration/ConfigurationValidatorImpl.java
@@ -6,23 +6,24 @@ import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ConfigurationValidatorImpl implements ConfigurationValidator {
     @Override
     public boolean validateConfigurationObject(Configuration configuration) {
         return configuration.steps() > 0
-                && configuration.landingSpot().equals(" ")
-                && checkAdjacentCoordinate(configuration.landingSpot())
+                && checkLandingSpots(configuration.map(), configuration.landingSpot())
+                && checkAdjacentCoordinate(configuration.map(), configuration.landingSpot())
                 && !configuration.map().isEmpty()
                 && checkSymbols(configuration.symbols());
     }
 
-    private boolean checkAdjacentCoordinate(Coordinate coordinate) {
+    private boolean checkAdjacentCoordinate(String map, Coordinate coordinate) {
         int x = coordinate.X();
         int y = coordinate.Y();
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                if (i != 0 && j != 0 && new Coordinate(x + i, y + j).equals(" ")) {
+                if (i != 0 && j != 0 && checkLandingSpots(map, new Coordinate(x + i, y + j))) {
                     return true;
                 }
             }
@@ -30,13 +31,13 @@ public class ConfigurationValidatorImpl implements ConfigurationValidator {
         return false;
     }
 
-    private boolean checkSymbols(List<String> symbols){
+    private boolean checkSymbols(List<String> symbols) {
         List<String> allSymbols = List.of("#", "&", "*", "%");
-        if(symbols.size() ==0){
+        if (symbols.size() == 0) {
             return false;
         }
-        for(String symbol: symbols){
-            if(!allSymbols.contains(symbol)){
+        for (String symbol : symbols) {
+            if (!allSymbols.contains(symbol)) {
                 return false;
             }
         }
@@ -44,9 +45,14 @@ public class ConfigurationValidatorImpl implements ConfigurationValidator {
         return true;
     }
 
-    private boolean checkLandingSpots(String[][] map, Coordinate coordinate){
-        int x=coordinate.X();
-        int y= coordinate.Y();
-        if(map[x][y])
+    public boolean checkLandingSpots(String map, Coordinate coordinate) {
+        int x = coordinate.X();
+        int y = coordinate.Y();
+
+//        return Objects.equals(map[x][y], " ");
+        int sqrtOfMapSize = (int) Math.sqrt(map.length());
+
+        String spot = map.split("")[x * sqrtOfMapSize + y]; //(x+1) daca incep de la 1 1
+        return Objects.equals(spot, " ");
     }
 }
