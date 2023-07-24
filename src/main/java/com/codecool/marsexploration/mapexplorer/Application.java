93a9e340318c5259e1aacc133964b059d062135a
Silvia Lupsa <lupsasilvia@gmail.com>
2023-07-24 10:38:07 +0300

    mapLoader implementation

--- src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
@@ -1,6 +1,11 @@
 package com.codecool.marsexploration.mapexplorer;
 
+import com.codecool.marsexploration.mapexplorer.maploader.MapLoader;
+import com.codecool.marsexploration.mapexplorer.maploader.MapLoaderImpl;
 import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Application {
     private static final String workDir = "src/main";
@@ -8,8 +13,10 @@ public class Application {
     public static void main(String[] args) {
         String mapFile = workDir + "/resources/exploration-0.map";
         Coordinate landingSpot = new Coordinate(6, 6);
-
+        MapLoader mapLoader= new MapLoaderImpl();
+        System.out.println(mapLoader.load(mapFile).toString());
         // Add your code here
     }
+
 }
 
--- src/main/java/com/codecool/marsexploration/mapexplorer/maploader/MapLoader.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/maploader/MapLoader.java
@@ -2,6 +2,11 @@ package com.codecool.marsexploration.mapexplorer.maploader;
 
 import com.codecool.marsexploration.mapexplorer.maploader.model.Map;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 public interface MapLoader {
     Map load(String mapFile);
+
 }
