488fcabaee6d33cecc32990b09c79d910d77ad7c
RaresButuc <116391767+RaresButuc@users.noreply.github.com>
2023-07-25 09:18:11 +0300

    Mars Rover

--- src/main/java/com/codecool/marsexploration/mapexplorer/rovers/model/MarsRover.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/rovers/model/MarsRover.java
@@ -5,13 +5,20 @@ import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
 import java.util.HashMap;
 
 public class MarsRover {
-    private final int id;
     private final Coordinate currentPosition;
     private final int sight;
     private final HashMap<String, Coordinate> resources;
 
-    public MarsRover(int id, Coordinate currentPosition, int sight, HashMap<String, Coordinate> resources) {
-        this.id = id;
+    private int id;
+    private String name;
+
+    private static int counter = 0;
+
+    public MarsRover(Coordinate currentPosition, int sight, HashMap<String, Coordinate> resources) {
+        this.id = counter;
+        counter++;
+
+        this.name = "rover-" + id;
         this.currentPosition = currentPosition;
         this.sight = sight;
         this.resources = resources;
@@ -19,6 +26,10 @@ public class MarsRover {
 
     public int getId() {
         return id;
+    }
+
+    public String getNamed() {
+        return name;
     }
 
     public Coordinate getCurrentPosition() {
