f15c4845287618f5bad2dbc1dd14a805a50163e1
Silvia Lupsa <lupsasilvia@gmail.com>
2023-07-27 16:06:22 +0300

    still working

--- src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
@@ -2,6 +2,7 @@ package com.codecool.marsexploration.mapexplorer;
 
 import com.codecool.marsexploration.mapexplorer.configuration.ConfigurationValidatorImpl;
 import com.codecool.marsexploration.mapexplorer.configuration.model.Configuration;
+import com.codecool.marsexploration.mapexplorer.logger.FileLogger;
 import com.codecool.marsexploration.mapexplorer.maploader.MapLoader;
 import com.codecool.marsexploration.mapexplorer.maploader.MapLoaderImpl;
 import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
@@ -17,29 +18,30 @@ public class Application {
     private static final String workDir = "src/main";
 
     public static void main(String[] args) {
-        
-        String mapFile = workDir + "/resources/exploration-0.map";
-        HashMap<String, List<Coordinate>> resources = new HashMap<>();
-        Coordinate landingSpot = new Coordinate(6, 6);
-        MapLoader mapLoader= new MapLoaderImpl();
-        ConfigurationValidatorImpl configurationValidator = new ConfigurationValidatorImpl();
-        Configuration mapConfiguration = new Configuration(mapFile, landingSpot, List.of("#", "&", "*", "%"), 30);
+        for(int i=0; i<3; i++){
+            String mapFile = workDir + "/resources/exploration-"+i+".map";
+            HashMap<String, List<Coordinate>> resources = new HashMap<>();
+            Coordinate landingSpot = new Coordinate(6, 6);
+            MapLoader mapLoader= new MapLoaderImpl();
+            ConfigurationValidatorImpl configurationValidator = new ConfigurationValidatorImpl();
+            Configuration mapConfiguration = new Configuration(mapFile, landingSpot, List.of("#", "&", "*", "%"), 30);
 
-
-        if(configurationValidator.checkLandingSpots(landingSpot,mapConfiguration)) {
-            InitializeRover initializeRover = new InitializeRover();
-            String mapContent=mapLoader.load(mapFile).toString();
+            if(configurationValidator.checkLandingSpots(landingSpot,mapConfiguration)) {
+                InitializeRover initializeRover = new InitializeRover();
+                String mapContent=mapLoader.load(mapFile).toString();
 
 //            HashMap<String, List<Coordinate>> resources = explorationSimulatorNotUsed.getResources(mapConfiguration);
-            MarsRover rover = initializeRover.initializeRover(landingSpot, 2, resources, mapConfiguration);
-            SimulationContext simulationContext = new SimulationContext(0, 300, rover, landingSpot, mapFile, resources);
+                MarsRover rover = initializeRover.initializeRover(landingSpot, 2, resources, mapConfiguration);
+                SimulationContext simulationContext = new SimulationContext(0, 60, rover, landingSpot, mapFile, resources);
 //        explorationSimulatorNotUsed.runSimulation(mapConfiguration, 2);
-            ExplorationSimulator explorationSimulator = new ExplorationSimulator(simulationContext,configurationValidator,mapConfiguration);
-            explorationSimulator.startExploring();
-//        explorationSimulator.getResources(mapConfiguration);
-            System.out.println(rover.getCurrentPosition());
-        }else {
-            System.out.println("Invalid landing Spot");}
+                FileLogger fileLogger = new FileLogger("src/main/resources/ResultsAfterExploration-"+i+".map");
+                ExplorationSimulator explorationSimulator = new ExplorationSimulator(fileLogger, simulationContext,configurationValidator,mapConfiguration);
+                explorationSimulator.startExploring();
+                System.out.println("File ResultsAfterExploration-"+i+".map successful created.");
+            }else {
+                System.out.println("Invalid landing Spot");
+            }
+        }
 
     }
 
--- src/main/java/com/codecool/marsexploration/mapexplorer/configuration/ConfigurationValidator.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/configuration/ConfigurationValidator.java
@@ -3,13 +3,12 @@ package com.codecool.marsexploration.mapexplorer.configuration;
 import com.codecool.marsexploration.mapexplorer.configuration.model.Configuration;
 import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
 
-import java.time.Duration;
 import java.util.List;
 
 public interface ConfigurationValidator {
     boolean validateConfigurationObject(Configuration configuration);
 
-    List<Coordinate> checkAdjacentCoordinate( Coordinate coordinate, Configuration configuration);
+    List<Coordinate> checkAdjacentCoordinate(Coordinate coordinate, Configuration configuration);
 
     boolean checkSymbols(List<String> symbols);
 
