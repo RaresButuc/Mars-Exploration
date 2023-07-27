ca259bab019db3e5694dbd7302b33bcbece4b9c7
Silvia Lupsa <lupsasilvia@gmail.com>
2023-07-27 14:54:00 +0300

    edited

--- src/main/java/com/codecool/marsexploration/mapexplorer/simulation/SuccessAnalyzer.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/simulation/SuccessAnalyzer.java
@@ -6,13 +6,22 @@ import com.codecool.marsexploration.mapexplorer.exploration.ExplorationOutcome;
 public class SuccessAnalyzer implements OutcomeAnalyzer {
     @Override
     public boolean hasReachedOutcome(SimulationContext context, Configuration configuration) {
-        // Implement your specific success condition for colonization here
-        // For example, check if conditions for colonization are met
-        // You can use methods from the ConfigurationValidator class or other utilities to check the conditions.
         context.setExplorationOutcome(ExplorationOutcome.COLONIZABLE);
-        return context.getMonitoredResources().keySet().contains("&")
-                && context.getMonitoredResources().keySet().contains("#")
-                && context.getMonitoredResources().keySet().contains("%")
-                && context.getMonitoredResources().keySet().contains("*");
+        boolean checked = false;
+        if (context.getMonitoredResources().containsKey("#") && context.getMonitoredResources().containsKey("&")) {
+            checked = (context.getMonitoredResources().get("#").size() >= 1 && context.getMonitoredResources().get("&").size() >= 1);
+        }
+
+        int sizeOfMineral = context.getMonitoredResources().get("%") != null ? context.getMonitoredResources().get("%").size() : 0;
+        int sizeOfWater = context.getMonitoredResources().get("*") != null ? context.getMonitoredResources().get("*").size() : 0;
+
+        context.setExplorationOutcome(ExplorationOutcome.COLONIZABLE);
+        return (context.getMonitoredResources().containsKey("&")
+                && context.getMonitoredResources().containsKey("#")
+                && context.getMonitoredResources().containsKey("%")
+                && context.getMonitoredResources().containsKey("*"))
+                || (sizeOfMineral > 4
+                && sizeOfWater > 3)
+                || (checked);
     }
 }
